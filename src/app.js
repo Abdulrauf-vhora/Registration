9ef5b7d42a590175b2d626e266206fc588d4f2fa
VhoraAbdulrauf <raufvhora4480@gmail.com>
2022-03-30 09:11:07 +0530

    ok

--- src/app.js
+++ src/app.js
@@ -3,28 +3,47 @@ const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
 const bcrypt = require('bcryptjs');
+const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 require('../src/db/conn');
 const Register = require('./models/register');
-
+const auth = require('./middleware/auth');
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
 const staticPath = path.join(__dirname, '../public');
 const templatePath = path.join(__dirname, '../templates/views');
 const partialPath = path.join(__dirname, '../templates/partials');
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(staticPath));
 app.set('view engine', 'hbs');
 app.set('views', templatePath);
 hbs.registerPartials(partialPath); // to use partials
 
 app.get('/', (req, res) => {
-  console.log('okok');
   res.render('index');
 });
+app.get('/secret', auth, (req, res) => {
+  res.render('secret');
+});
+app.get('/logout', auth, async (req, res) => {
+  try {
+    // for remove single token /logout from single device
+    // req.user.tokens = req.user.tokens.filter((currentElement) => {
+    //   return currentElement.token !== req.token;
+    // });
+    // req.token i.e. current token
+    req.user.tokens = []; // logout from all devices
+    res.clearCookie('jwt');
+    await req.user.save();
+    res.render('login');
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
 app.get('/register', (req, res) => {
   res.render('register');
 });
@@ -48,7 +67,17 @@ app.post('/register', async (req, res) => {
       });
       console.log('success part', registerEmp);
       const token = await registerEmp.generateAuthToken();
-      console.log('token part', token);
+      console.log('reg token part', token);
+
+      // The res.cookie() function is used to set the cookie name to value.
+      // The value parameter may be a string or object converted to json
+
+      // syntax
+      // res.cookie(name,value,[Options])
+      res.cookie('jwt', token, {
+        expires: new Date(Date.now() + 600000),
+        httpOnly: true,
+      });
 
       const registered = await registerEmp.save();
       console.log('registered part', registered);
@@ -71,7 +100,14 @@ app.post('/login', async (req, res) => {
     const userEmail = await Register.findOne({ email: email });
     const isMatch = await bcrypt.compare(password, userEmail.password);
     const token = await userEmail.generateAuthToken();
+
     console.log('login token part', token);
+    res.cookie('jwt', token, {
+      expires: new Date(Date.now() + 600000),
+      httpOnly: true,
+    });
+    // console.log(`cookis ${req.cookies.jwt}`);
+
     console.log(isMatch);
     if (isMatch) {
       // res.status(201).render('index');
@@ -110,3 +146,6 @@ app.post('/login', async (req, res) => {
 app.listen(port, () => {
   console.log(`server is running at port : ${port}`);
 });
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjQyZjBiZmIyZTBkMmY3YzIwMDMzMjAiLCJpYXQiOjE2NDg1NTg5MDN9.1vCn9sfIAr9ksty59jDJ7r0Z-1tT-0j2MCevIhnTarI
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjQyZjBiZmIyZTBkMmY3YzIwMDMzMjAiLCJpYXQiOjE2NDg1NTg4Nzd9.4YoE2RLoR4jDh5h1ic5v4UWCytYqgawpstfzb0a-HTc
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjQyZjBiZmIyZTBkMmY3YzIwMDMzMjAiLCJpYXQiOjE2NDg1NTg5MDN9.1vCn9sfIAr9ksty59jDJ7r0Z-1tT-0j2MCevIhnTarI
